f851681451859a31c0cc202dd7e77dcd75dc150d
Andrew Manitsas <andrewmanitsas@gmail.com>
2017-10-15 11:23:44 +0300

    java working

--- UniversalHandler.java
+++ UniversalHandler.java
@@ -1,5 +1,3 @@
-package homehub;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
---
+++ programm.py
@@ -0,0 +1,11 @@
+import random
+import time
+
+#file_electricity = "sensors/electricity.txt"
+file_heater_temp = "sensors/heater_temperature.txt"
+file_humidity = "sensors/humidity.txt"
+file_temperature = "sensors/temperature.txt"
+
+error_file_heater = "site_values/error_heater.txt"
+error_file_humidity = "site_values/error_humidity.txt"
+error_file_temperature = "site_values/error_temperature.txt"
